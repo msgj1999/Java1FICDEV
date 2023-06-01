b094b623671df065854378eae27b51bb83142b20
msgj1999 <135090592+msgj1999@users.noreply.github.com>
2023-06-01 08:50:44 -0400

    Envio de teste

---
+++ teste2/src/teste2/Teste2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teste2;
+
+/**
+ *
+ * @author ficdev
+ */
+public class Teste2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ teste2/src/teste2/tstedoteste.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package teste2;
+
+/**
+ *
+ * @author ficdev
+ */
+public class tstedoteste {
+    th5ehe5u56u
+}
